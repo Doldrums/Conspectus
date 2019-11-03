df7d3975ff45ef1de75552ced26b16faaa8ad8d7
Albert Akmukhametov <albert@kinjalik.ru>
2019-11-03 16:20:11 +0500

    Raw slider done

--- School/Web Development/Assignment_1/scripts/main.js
+++ School/Web Development/Assignment_1/scripts/main.js
@@ -1,2 +1,7 @@
+import Glide from '@glidejs/glide'
 import "normalize.css";
+import "@glidejs/glide/src/assets/sass/glide.core"
+import "@glidejs/glide/src/assets/sass/glide.theme"
 import "./../styles/main.scss";
+
+new Glide("#banner-carousel").mount();
